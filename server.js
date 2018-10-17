50cb3e087d8be59c29246c6b3cc223b0d80d71fe
Chris Kozlowski <kozlowskicd@gmail.com>
2018-10-16 19:44:27 -0700

    built express server, added home and catagories routes

--- server.js
+++ server.js
@@ -10,11 +10,23 @@ const API = process.env.API_URL;
 app.set('view engine', 'ejs');
 app.use(express.static('./public'));
 
-app.get('/', (rec, res) => {
-  res.render('index');
-});
+app.get('/', renderHomepage);
+app.get('/categories', renderAllCategories);
+app.get('/categories:name', renderTheseProducts);
 
+function renderHomepage(req, res) {
+  res.render('site', {page:'./pages/index', title:'Homepage'});
+}
+function renderAllCategories(req, res) {
+  superagent.get(`${API}/categories`)
+    .then(data => {
+      res.render('site', {page:'./pages/categories', title:'All Categories', categories:data.body});
+    })
+    .catch(err => console.log(err));
+}
+function renderTheseProducts(req, res) {
+  res.render('site', {page:'./pages/products', title:'Products'});
+}
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
 });
-
